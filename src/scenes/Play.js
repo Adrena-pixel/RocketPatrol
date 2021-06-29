5078fab347abbef7f47c1461e0f0a53adf22ce81
Yongbo Wang <ywang682@ucsc.edu>
2021-06-29 23:27:48 +0800

    bouns ship added

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -28,7 +28,7 @@ class Play extends Phaser.Scene {
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
-        this.ship04 = new Bonusship(this, game.config.width + borderUISize*9, borderUISize*3, 'bonus_ship', 0, 80).setOrigin(0, 0);
+        this.ship04 = new Bonusship(this, game.config.width + borderUISize*9, borderUISize*6 + borderPadding*8, 'bonus_ship', 0, 80).setOrigin(0, 0);
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
